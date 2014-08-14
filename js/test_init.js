8bc2175421f13a60211fd24b567cc7cb86777041
eugen <eugen@jaja.de>
2014-08-14 06:57:04 +0200

    adopted test_init for the new changes

--- js/test_init.js
+++ js/test_init.js
@@ -1,2 +1,2 @@
 	//for testing, this is not required if you dont call
-	var e2c_id=5190; //write the ID here manually after connecting a user, so ther views are accessible
+	var e2c_id=6171; //write the ID here manually after connecting a user, so ther views are accessible
