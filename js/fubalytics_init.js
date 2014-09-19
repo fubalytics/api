cb6ad06486141077898b37f1b722fdfe7d3f7cee
evgfedko <evgfedko@evgfedko-ubuntu>
2014-09-19 19:14:40 +0200

    files deleted, fubalytics.js updated

--- js/fubalytics_init.js
+++
@@ -1,5 +0,0 @@
-	//use this file to configure the fubalytics system instead of editing the fubalytics js file directly
-	//= require fubalytics.js
-	
-	fubalytics.auth_token="pB8uKyK6SrqqpHDBzU2X";
-	fubalytics.fubalytics_url="http://testing.fubalytics.local:3000";
