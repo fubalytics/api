1d4144cc7b56d42d82219ff3905e6d95d1b6a5cc
Eugen <ef>
2016-05-06 14:15:02 +0200

    fixed player view

--- js/test_init.js
+++ js/test_init.js
@@ -1,8 +1,4 @@
 	//for testing, this is not required if you dont call
-<<<<<<< HEAD
-	var e2c_id=6639; //write the ID here manually after connecting a user, so ther views are accessible
-=======
-	
 	//run the view connect.html first in order to get this e2c_id
-	var e2c_id=2598; //write the ID here manually after connecting a user, so the views are accessible
->>>>>>> 2ecd3c4d549f7752825a59ef526be4c1521f07a5
+	var e2c_id=120834; //write the ID here manually after connecting a user, so the views are accessible
+
